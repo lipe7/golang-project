b327263469076ce39d26c761eb1e52521afb3e49
felipe <felipec165@gmail.com>
2024-02-02 16:42:27 -0300

    add routes

--- router/routes.go
+++ router/routes.go
@@ -7,11 +7,35 @@ import (
 )
 
 func initializeRoutes(router *gin.Engine) {
-	v1 := router.Group("/api")
+	v1 := router.Group("/api/v1")
 
-	v1.GET("opening", func(ctx *gin.Context) {
+	v1.GET("/opening", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{
 			"status": "opening",
+		})
+	})
+
+	v1.POST("/opening", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"status": "POST opening",
+		})
+	})
+
+	v1.DELETE("/opening", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"status": "DELETE opening",
+		})
+	})
+
+	v1.PUT("/opening", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"status": "PUT opening",
+		})
+	})
+
+	v1.GET("/openings", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"status": "openings",
 		})
 	})
 }
